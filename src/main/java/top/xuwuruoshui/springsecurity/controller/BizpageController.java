ffd6c017bb7517e27569d3f68a206fdb2fba4d5f
xuwuruoshui <1085252985@qq.com>
2020-05-13 21:22:07 +0800

    rbac

--- src/main/java/top/xuwuruoshui/springsecurity/controller/BizpageController.java
+++ src/main/java/top/xuwuruoshui/springsecurity/controller/BizpageController.java
@@ -3,19 +3,16 @@ package top.xuwuruoshui.springsecurity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import top.xuwuruoshui.springsecurity.service.MethodELService;
-
-import java.util.List;
 
 @Controller
 public class BizpageController {
 
-    private final MethodELService methodELService;
+    /*private final MethodELService methodELService;
 
     public BizpageController(MethodELService methodELService) {
         this.methodELService = methodELService;
     }
-
+*/
 
     // 登录
     @PostMapping("/login")
@@ -33,13 +30,13 @@ public class BizpageController {
     @GetMapping("/syslog")
     public String showOrder() {
         //测试@EnableGlobalMethodSecurity全局方法
-        //methodELService.findAll();
-        //methodELService.findOne();
-//        List<Integer> ids = new ArrayList<>();
-//        ids.add(1);
-//        ids.add(2);
-//        methodELService.delete(ids,null);
-        List<MethodELService.PersonDemo> pds = methodELService.findAllPD();
+       /* methodELService.findAll();
+        methodELService.findOne();
+        List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        methodELService.delete(ids,null);
+        List<MethodELService.PersonDemo> pds = methodELService.findAllPD();*/
         return "syslog";
     }
 
